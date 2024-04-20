36a7014ff7ecf154f7b5f4765027eeb72ef9fc29
Mina Basem <42482261+MinaBasem@users.noreply.github.com>
2024-04-21 01:45:06 +0200

    Version 1.1
    - Added Try and Except statements
    - Added toolbar
    - Added save and open .csv options
    - Replaced '@email' with various options
    - Added Icon

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import requests
 import json
 import pandas as pd
+import datetime
+import random
 from pandas import json_normalize
-from PyQt5.QtCore import (Qt, QSize)
+from PyQt5.QtCore import (Qt, QSize, QUrl)
 from PyQt5.QtGui import QIcon
 from PyQt5.QtWidgets import (
     QApplication,
@@ -19,14 +21,43 @@ from PyQt5.QtWidgets import (
     QTableWidgetItem,
     QListWidget,
     QSpinBox,
-    QAbstractItemView
+    QAbstractItemView,
+    QFileDialog,
+    QMessageBox
 )
+
+from misc.messages import help_text # type: ignore
 
 class MainWindow(QMainWindow):
     def __init__(self):
+        self.dataframe = pd.DataFrame()
         super(MainWindow, self).__init__()
-        self.setWindowTitle("MockMeister v1.0")
+        self.setWindowTitle("MockMeister v1.1")
         self.setFixedSize(800, 500)
+        icon = QIcon("misc/mockmeister_simple_logo.png")
+        self.setWindowIcon(icon)
+
+        toolbar = QToolBar("Toolbar")
+        toolbar.setIconSize(QSize(16, 16))
+        self.addToolBar(toolbar)
+        toolbar.setMovable(False)
+
+        self.save_button = QAction("Save", self)
+        self.save_button.triggered.connect(self.save_file)
+
+        self.open_button = QAction("Open", self)
+        self.open_button.triggered.connect(self.open_file)
+
+        self.help_button = QAction("Help", self)
+        self.help_button.triggered.connect(self.show_help)
+
+        #self.about_button = QAction("About", self)
+        #self.about_button.triggered.connect(self.show_about)
+
+        toolbar.addAction(self.save_button)
+        toolbar.addAction(self.open_button)
+        toolbar.addAction(self.help_button)
+        #toolbar.addAction(self.about_button)
 
         V_layout = QVBoxLayout()
         H_layout = QHBoxLayout()
@@ -52,7 +83,7 @@ class MainWindow(QMainWindow):
 
         self.table = QTableWidget()
         self.table.setRowCount(15)                   # Set row count based on data
-        self.table.setColumnCount(6)                        # Set column count based on data keys
+        self.table.setColumnCount(6)                 # Set column count based on data keys
         #self.table.setHorizontalHeaderLabels(list(data.keys()))     # Set column headers
 
 
@@ -65,8 +96,9 @@ class MainWindow(QMainWindow):
         self.table.setAcceptDrops(True)
         self.table.setDropIndicatorShown(True)
 
-        generate_button = QPushButton("Generate")
-        generate_button.clicked.connect(self.generate_table)
+        self.generate_button = QPushButton("Generate")
+        self.generate_button.setEnabled(False)
+        self.generate_button.clicked.connect(self.generate_table)
         add_button = QPushButton("+")
         add_button.setFixedWidth(20)
         add_button.clicked.connect(self.add_button_func)
@@ -81,18 +113,22 @@ class MainWindow(QMainWindow):
         Left_Column_layout.addLayout(Top_Left_Column_layout)
         Left_Column_layout.addWidget(self.list_widget)
         Left_Column_layout.addLayout(self.Bottom_Left_Column_layout)
-        Left_Column_layout.addWidget(generate_button)
+        Left_Column_layout.addWidget(self.generate_button)
         H_layout.addLayout(Left_Column_layout)
         H_layout.addWidget(self.table)
         V_layout.addLayout(H_layout, stretch=8)
 
-        V_layout.addWidget(QLabel("@MinaBasem"))
+        self.save_as_csv_button = QPushButton("Export as CSV")
+        self.save_as_csv_button.clicked.connect(self.save_file)
+        V_layout.addWidget(self.save_as_csv_button)
 
         widget = QWidget()
         widget.setLayout(V_layout)
         self.setCentralWidget(widget)
 
+
     def add_button_func(self):
+        self.generate_button.setEnabled(True)
         combo_box_selected_text = self.combo_box.currentText()
         self.list_widget.addItem(combo_box_selected_text)
 
@@ -104,6 +140,7 @@ class MainWindow(QMainWindow):
             self.list_widget.addItem(item)
 
     def remove_button_func(self):
+        
         selected_item = self.list_widget.currentItem()
         if selected_item:
             row = self.list_widget.currentRow()
@@ -127,29 +164,117 @@ class MainWindow(QMainWindow):
         data = json.loads(response.text)
         df = json_normalize(data)
         needed_data = self.get_all_items()
-        print(df[needed_data])
-        return df[needed_data]
+
+        df = self.transform_data(df, needed_data)
+
+        self.dataframe = df[needed_data]
+        print(self.dataframe)
+        return self.dataframe
+    
+    def transform_data(self, df, needed_data):
+        if 'email' in needed_data:
+            domain_options = ['@gmail', '@yahoo', '@hotmail']
+            def replace_email(email):
+                return email.replace('@email', random.choice(domain_options))
+            df['email'] = df['email'].apply(replace_email)
+
+            separator_options = ['_', '.']
+            def replace_separator(email):
+                return email.replace('.', random.choice(separator_options), 1)
+            df['email'] = df['email'].apply(replace_separator)
+
+            def add_random_number_to_email(email):
+                add_or_not = random.choice([0, 1])
+                if add_or_not == 0:
+                    return email
+                else:
+                    random_number_str = str(random.randint(0, 9999)).zfill(4)
+                    email_parts = email.split('@')
+                    modified_username = email_parts[0] + "_" + random_number_str
+                    return modified_username + "@" + email_parts[1]
+            df['email'] = df['email'].apply(add_random_number_to_email)
+
+        return df
 
 
-    def generate_table(self):
-        new_data = self.generate_data()
+    def generate_table(self):           
+        try:
+            new_data = self.generate_data()
+            #self.save_file(new_data)
 
-        self.table.clearContents()          # Clear the existing table
-        self.table.setRowCount(0)
-        self.table.setColumnCount(0)
+            self.table.clearContents()          # Clear the existing table
+            self.table.setRowCount(0)
+            self.table.setColumnCount(0)
 
-        # Set the table rows and columns count depending on dataframe
-        self.table.setRowCount(len(new_data))
-        self.table.setColumnCount(len(new_data.columns))
+            # Set the table rows and columns count depending on dataframe
+            self.table.setRowCount(len(new_data))
+            self.table.setColumnCount(len(new_data.columns))
 
-        # Set the horizontal header labels from the DataFrame column names
-        self.table.setHorizontalHeaderLabels(list(new_data.columns))
+            # Set the horizontal header labels from the DataFrame column names
+            self.table.setHorizontalHeaderLabels(list(new_data.columns))
 
-        # Populate the table cells with data from the DataFrame
-        for row, row_data in new_data.iterrows():
-            for col, value in enumerate(row_data):
-                item = QTableWidgetItem(str(value))  # Convert value to string for display
-                self.table.setItem(row, col, item)
+            # Populate the table cells with data from the DataFrame
+            for row, row_data in new_data.iterrows():
+                for col, value in enumerate(row_data):
+                    item = QTableWidgetItem(str(value))  # Convert value to string for display
+                    self.table.setItem(row, col, item)
+
+        except:
+            message_box = QMessageBox(self)     # Shows python3 in title, fix it
+            message_box.setText("Error calling API and generating table. \nPlease retry.")
+            message_box.setIcon(QMessageBox.Icon.Warning)
+            message_box.exec()
+
+
+    def save_file(self):
+        now = datetime.datetime.now()
+        formatted_datetime = now.strftime("%d-%m-%Y-%H-%M-%S")
+        self.dataframe.to_csv("data-" + formatted_datetime + ".csv", index=False)
+        print("Data saved to location")
+
+    def open_file(self):
+        options = QFileDialog.Options()
+        options |= QFileDialog.Option.ShowDirsOnly
+        filename, _ = QFileDialog.getOpenFileName(self, "Open File", "", "CSV Files (*.csv)", options=options)
+
+        if filename:
+            try:
+                new_data = pd.read_csv(filename)
+                self.table.clearContents()
+                self.table.setRowCount(0)
+                self.table.setColumnCount(0)
+                self.table.setRowCount(len(new_data))
+                self.table.setColumnCount(len(new_data.columns))
+                self.table.setHorizontalHeaderLabels(list(new_data.columns))
+
+                for row, row_data in new_data.iterrows():
+                    for col, value in enumerate(row_data):
+                        item = QTableWidgetItem(str(value)) 
+                        self.table.setItem(row, col, item)
+
+            except pd.errors.ParserError:
+                print("Error parsing CSV file.")
+
+    def show_help(self):
+        help_box = QMessageBox(self)     # Shows python3 in title, fix it
+        help_box.setWindowTitle("Help")
+        help_box.setText(help_text)
+        help_box.setIcon(QMessageBox.Icon.Information)
+        help_box.exec()
+
+    """def show_about(self):
+        help_box = QMessageBox(self)
+        about_text = "MockMeister v1.1 \nhttps://github.com/MinaBasem/MockMeister"
+        help_box.setText(about_text)
+        help_box.setIcon(QMessageBox.Icon.Warning)
+        help_box.exec()"""
+
+    """def open_url(self, url):
+        try:
+            from PyQt5.QtCore import QUrl, QDesktopServices
+            QDesktopServices.openUrl(QUrl(url))
+        except:
+            QMessageBox.critical(self, "Error", "Failed to open link.")"""
 
 if __name__ == "__main__":
     app = QApplication([])
---
+++ misc/messages.py
@@ -0,0 +1,27 @@
+help_text = """MockMeister is a desktop application that helps you generate random mock data for various purposes like testing or creating sample datasets. Here's a quick guide to get you started:
+
+1. Choosing Data Fields:
+
+On the left side, you'll find a list widget. This is where you select the data fields you want to generate.
+Click the dropdown menu in the "Combo Box" section to see a list of available data fields (e.g., first name, last name, email, etc.).
+Click the "+" button to add the selected field to the list widget.
+You can add multiple fields by selecting them from the dropdown and clicking "+" repeatedly.
+The "-" button allows you to remove unwanted fields from the list.
+
+2. Specifying Number of Rows:
+
+In the "Row count:" section, use the spin box to set the desired number of rows for your generated data.
+The minimum value is 1, and the maximum is 100.
+
+3. Generating Data:
+
+Once you've chosen your data fields and row count, click the "Generate" button.
+MockMeister will fetch random data from an online API and populate the table on the right side.
+The table will have columns corresponding to the selected data fields and rows based on the specified count.
+
+4. Saving and Loading Data:
+
+Click the "Save" button (in the toolbar) to save the generated data to a CSV file on your computer.
+The filename will include a timestamp for easy identification.
+Click the "Open" button (in the toolbar) to load previously saved data from a CSV file.
+MockMeister will display the loaded data in the table."""
